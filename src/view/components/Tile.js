1c267627263fd70c97a4e4c437aba2ed91b90c8a
james0248 <hsjames0248@gmail.com>
2020-03-10 21:20:59 +0900

    Add more schools

--- src/view/components/Tile.js
+++ src/view/components/Tile.js
@@ -1,29 +1,22 @@
 import React, { Component, createRef } from 'react'
 import gsap from 'gsap'
 
-import logo_1 from '../../public/images/한과영.png'
-import logo_2 from '../../public/images/광주.png'
-import logo_3 from '../../public/images/대구.png'
-import logo_4 from '../../public/images/대전.png'
-import logo_5 from '../../public/images/세종예술.png'
-import logo_6 from '../../public/images/경곽.png'
-import logo_7 from '../../public/images/인천.png'
-import logo_8 from '../../public/images/세곽.png'
-import logo_9 from '../../public/images/한곽.png'
-import logo_10 from '../../public/images/설곽.png'
+import logo_1 from '../../public/images/한국과학영재교.png'
+import logo_2 from '../../public/images/광주영재교.png'
+import logo_3 from '../../public/images/대구영재교.png'
+import logo_4 from '../../public/images/대전영재교.png'
+import logo_5 from '../../public/images/세종예술영재교.png'
+import logo_6 from '../../public/images/경기영재교.png'
+import logo_7 from '../../public/images/인천예술영재교.png'
+import logo_8 from '../../public/images/민사고.png'
+import logo_9 from '../../public/images/세종과학고.png'
+import logo_10 from '../../public/images/한성과학고.png'
+import logo_11 from '../../public/images/서울국제고.png'
+import logo_12 from '../../public/images/서울영재교.png'
 
-const image = {
-    1: logo_1,
-    2: logo_2,
-    3: logo_3,
-    4: logo_4,
-    5: logo_5,
-    6: logo_6,
-    7: logo_7,
-    8: logo_8,
-    9: logo_9,
-    10: logo_10,
-}
+const image = [
+    logo_1, logo_2, logo_3, logo_4, logo_5, logo_6, logo_7, logo_8, logo_9, logo_10, logo_11, logo_12
+]
 
 class Tile extends Component {
     constructor(props) {
@@ -32,10 +25,10 @@ class Tile extends Component {
     }
 
     componentDidUpdate() {
-        if(this.props.tile.isNew) {
+        if (this.props.tile.isNew) {
             gsap.fromTo(this.ref.current, { scale: 0 }, { scale: 1, duration: 0.2 })
         }
-        if(this.props.tile.isMerged) {
+        if (this.props.tile.isMerged) {
             gsap.fromTo(this.ref.current, { scale: 1 }, {
                 scale: 1.1,
                 duration: 0.08,
@@ -46,7 +39,7 @@ class Tile extends Component {
     }
 
     render() {
-        if(this.props.tile.rank === 0) {
+        if (this.props.tile.rank === 0) {
             return (
                 <div
                     className='empty tile'
