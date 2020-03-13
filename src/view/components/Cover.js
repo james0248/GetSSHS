1a662257bf1847f68673dfce390352446ee0cf09
james0248 <hsjames0248@gmail.com>
2020-03-13 19:06:17 +0900

    Add submitting name function

--- src/view/components/Cover.js
+++ src/view/components/Cover.js
@@ -1,29 +1,90 @@
 import React, { Component, createRef } from 'react'
 import gsap from 'gsap'
+import TextInput from './TextInput'
+import success from '../../public/images/check.svg'
+import fail from '../../public/images/x.svg'
+
+const style = {
+    width: '30%',
+    height: '100%',
+}
 
 class Cover extends Component {
     constructor(props) {
         super(props)
+        let defaultName = window.localStorage.getItem('name')
+        defaultName = defaultName === null ? '' : defaultName
+        this.state = {
+            name: defaultName,
+            display: true,
+        }
         this.ref = createRef()
+        this.submitRef = createRef()
+        this.onChange = this.onChange.bind(this)
     }
 
     componentDidUpdate() {
-        if (this.props.display) {
+        if (this.props.display && this.state.display) {
             gsap.fromTo(this.ref.current, { opacity: 0 }, {
+                delay: 0.5,
                 opacity: 1,
-                duration: 2,
+                duration: 1.5,
+                onComplete: () => {
+                    this.setState({ display: false })
+                }
             })
         }
     }
 
+    onChange(event) {
+        const value = event.target.value;
+        this.setState({ name: value });
+    }
+
     render() {
+        let submitButton = (
+            <a
+                id='submit'
+                className='button'
+                onClick={(e) => {
+                    e.preventDefault();
+                    this.props.handleSubmit(this.state.name)
+                }}
+            >Submit</a>
+        )
+        if(this.props.success === 1) {
+            submitButton = (
+                <img
+                    style={style}
+                    src={success}
+                ></img>
+            )
+        } else if(this.props.success === -1){
+            submitButton = (
+                <img
+                    style={style}
+                    src={fail}
+                ></img>
+            )
+        }
         if (this.props.display) {
             return (
                 <div className='grid-cover' ref={this.ref}>
                     <p className='game-over'>Game Over!</p>
                     <div className='score'>You Scored {this.props.score} points!</div>
-                    <input type='text' id='name' defaultValue='Your name'></input>
-                    <a className='button' onClick={this.props.handleRetry}>Retry</a>
+                    <div className='ranking'>
+                        <TextInput
+                            id='name'
+                            label='이름을 입력하세요'
+                            onChange={this.onChange}
+                            value={this.state.name}
+                        />
+                        {submitButton}
+                    </div>
+                    <a className='button' onClick={() => {
+                        this.setState({ display: true });
+                        this.props.handleRetry()
+                    }}>Retry</a>
                 </div>
             )
         }
---
+++ src/view/components/TextInput.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class TextInput extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            focussed: props.focussed,
+            error: props.error || '',
+        }
+    }
+
+    render() {
+        const { focussed } = this.state;
+        const { id, value, label } = this.props;
+        const fieldValue = `field ${(focussed || value) && 'focussed'}`
+        return (
+            <div className={fieldValue}>
+                <input
+                    id={id}
+                    type="text"
+                    value={value}
+                    placeholder={label}
+                    onChange={this.props.onChange}
+                />
+            </div>
+        )
+    }
+}
+
+TextInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    label: PropTypes.string,
+    onChange: PropTypes.func,
+}
+
+export default TextInput
